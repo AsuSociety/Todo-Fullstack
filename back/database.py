b19d7881137a13cda31b305a92335c14826d0ab7
AsuSociety <omerasus3@gmail.com>
2024-08-12 14:45:55 +0300

    edit DB url

--- back/database.py
+++ back/database.py
@@ -6,6 +6,7 @@ import os
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL =os.getenv("DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL =os.getenv("VERCEL_URL")
 
 # print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
