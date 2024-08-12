564dfbf7d84b0934ba901dd07b8089c1bdd1d3d1
AsuSociety <omerasus3@gmail.com>
2024-08-12 15:51:09 +0300

    fix bugs

--- back/database.py
+++ back/database.py
@@ -10,6 +10,5 @@ SQLALCHEMY_DATABASE_URL =os.getenv("DATABASE_URL")
 # print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
